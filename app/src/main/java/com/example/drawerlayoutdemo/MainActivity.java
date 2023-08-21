f927ca5d6311e9473c63d775529e245c3838a1cf
KuroNekoLi <alice.margatroid.love@gmail.com>
2023-08-21 18:43:18 +0800

    完整版

--- app/src/main/java/com/example/drawerlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/drawerlayoutdemo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -13,7 +14,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -26,22 +32,25 @@ public class MainActivity extends AppCompatActivity
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        // 移除 ActionBar 的標題
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        // 設置自定義的返回圖標
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_menu_n);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        androidx.appcompat.widget.Toolbar customToolbar = findViewById( R.id.custom_toolbar );
+        setSupportActionBar(customToolbar);
 
 
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
         appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home)
+                R.id.nav_home,R.id.nav_settings)
                 .setOpenableLayout( drawerLayout )
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // 移除 ActionBar 的標題
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // 不使用預設的箭頭
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        // 不使用預設的漢堡圖
+        getSupportActionBar().setHomeButtonEnabled(false);
 
         //设置左侧菜单
         NavigationView navigationView = findViewById(R.id.navigation_view);
@@ -52,8 +61,47 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onDestinationChanged( @NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments)
             {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                getSupportActionBar().setHomeButtonEnabled(false);
+
+                ImageView customHamburgerIcon = customToolbar.findViewById(R.id.custom_hamburger_icon);
+                TextView fragmentTitle = customToolbar.findViewById(R.id.fragment_title);
+
+                if(destination.getId() == R.id.nav_home || destination.getId() == R.id.nav_settings) {
+                    customHamburgerIcon.setImageResource(R.drawable.icon_menu_n);  // 漢堡圖示
+                } else {
+                    customHamburgerIcon.setImageResource(R.drawable.icon_back_n);  // 返回圖示
+                }
+
+                // Set the title based on the current fragment
+                CharSequence label = destination.getLabel();
+                if (label != null) {
+                    fragmentTitle.setText(label);
+                }
             }
         });
+
+        ImageView customHamburgerIcon = customToolbar.findViewById(R.id.custom_hamburger_icon);
+        customHamburgerIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavDestination currentDestination = navController.getCurrentDestination();
+                if (currentDestination != null) {
+                    if (currentDestination.getId() == R.id.nav_home || currentDestination.getId() == R.id.nav_settings) {
+                        // 開啟或關閉 drawer
+                        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                        } else {
+                            drawerLayout.openDrawer(GravityCompat.START);
+                        }
+                    } else {
+                        // 返回上一個 Fragment
+                        navController.navigateUp();
+                    }
+                }
+            }
+        });
+
     }
 
     /**
--- app/src/main/java/com/example/drawerlayoutdemo/ProfileFragment.java
+++ app/src/main/java/com/example/drawerlayoutdemo/ProfileFragment.java
@@ -2,62 +2,29 @@ package com.example.drawerlayoutdemo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment
 {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment()
-    {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance( String param1, String param2 )
-    {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString( ARG_PARAM1, param1 );
-        args.putString( ARG_PARAM2, param2 );
-        fragment.setArguments( args );
-        return fragment;
-    }
-
     @Override
-    public void onCreate( Bundle savedInstanceState )
+    public void onViewCreated(
+            @NonNull View view, @Nullable Bundle savedInstanceState )
     {
-        super.onCreate( savedInstanceState );
-        if ( getArguments() != null )
-        {
-            mParam1 = getArguments().getString( ARG_PARAM1 );
-            mParam2 = getArguments().getString( ARG_PARAM2 );
-        }
+        super.onViewCreated( view, savedInstanceState );
+
+        Button btnDetail = view.findViewById( R.id.btn_detail );
+        btnDetail.setOnClickListener( v->{
+            Navigation.findNavController(v).navigate( R.id.action_nav_profile_to_profileDetailFragment );
+        } );
     }
 
     @Override
